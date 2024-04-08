699a6afbc739614663a3e0192cb65199386b5ee2
AlezinDS <alefoz09@hotmail.com>
2024-04-08 19:35:40 -0300

    Arquivo Spring - Carregado ao Repositório

---
+++ SIGIPE/src/main/java/com/SIGIPE/SIGIPE/SigipeApplication.java
@@ -0,0 +1,13 @@
+package com.SIGIPE.SIGIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigipeApplication.class, args);
+	}
+
+}
---
+++ SIGIPE/src/test/java/com/SIGIPE/SIGIPE/SigipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SIGIPE.SIGIPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
