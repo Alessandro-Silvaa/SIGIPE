ff36f555eaf712f7400d3d8c3aa42a9defe18150
AlezinDS <alefoz09@hotmail.com>
2024-04-09 19:44:18 -0300

    Entity-Atualizada

--- SIGIPE/src/main/java/app/entity/Curso_Demanda.java
+++
@@ -1,5 +0,0 @@
-	package app.entity;
-
-public class Curso_Demanda {
-
-}
--- SIGIPE/src/main/java/app/entity/Funcao.java
+++ SIGIPE/src/main/java/app/entity/Funcao.java
@@ -1,5 +1,30 @@
 package app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+
+
 public class Funcao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@NotBlank(message = "Campo Nome não pode ser nulo!")
+	private String nome;
+	
 
 }
