f33c8659dadc06cb35310c0b8faeee37a16e06fb
Saler C. Lourenço <saler.cez@gmail.com>
2024-04-09 21:58:04 -0300

    cria na controller findAll e findById

--- SIGIPE/src/main/java/app/service/DemandaService.java
+++ SIGIPE/src/main/java/app/service/DemandaService.java
@@ -1,12 +1,25 @@
 package app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.entity.Demanda;
 import app.repository.DemandaRepository;
 
 @Service
 public class DemandaService {
 	@Autowired
 	DemandaRepository demandaRepository;
+
+	public List<Demanda> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Demanda findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
